9f15b45d7e90d4f035429d56241dbe62b7d608a7
Jerod A. Merkle <jmerkle@uwyo.edu>
2019-06-12 11:59:10 -0600

    updates

--- code2run.r
+++ code2run.r
@@ -13,30 +13,52 @@ N0=50 # starting number of individuals
 t=100 # time of simulation
 sex.ratio=0.5 #what is the sex ratio of of the population/births?
 age.distr.lamba=5 # lambda value for starting age distribution based on poison distribution
+informed.distr.beta=c(.5, 1) # probability of knowing information, beta distribution ranges from 0 to 1 (vector of 2 values: shape1 and shape2)
+bold.distr.beta=c(2, 2) # probability of being bold, beta distribution (vector of 2 values: shape1 and shape2)
 birthdeath.file="C:/Users/jmerkle/Documents/GitHub/Memory_IBM/ageClass_Test.csv" #dataframe of age based birth and death rate
+result.folder="C:/Users/jmerkle/Desktop/results" #an empty folder where results will be saved.
+set_seed=TRUE # want to make results reproducible? Then set as TRUE
+save_at_each_iter=TRUE #should it write all results to file at each time step?
 vertTransmission=0
 
 
-results <- info.transfer.IBM()
+info.transfer.IBM(h=0.10, #increase in probability of death for uninformed
+                  nl=0.01, # naive learning probability
+                  si=10, # social interaction probability
+                  K=100, # carrying capacity
+                  N0=50, # starting number of individuals
+                  t=10, # time of simulation
+                  sex.ratio=0.5, #what is the sex ratio of of the population/births?
+                  age.distr.lamba=5, # lambda value for starting age distribution based on poison distribution
+                  informed.distr.beta=c(.5, 1), # probability of knowing information, beta distribution ranges from 0 to 1 (vector of 2 values: shape1 and shape2)
+                  bold.distr.beta=c(2, 2), # probability of being bold, beta distribution (vector of 2 values: shape1 and shape2)
+                  birthdeath.file="C:/Users/jmerkle/Documents/GitHub/Memory_IBM/ageClass_Test.csv", #dataframe of age based birth and death rate
+                  result.folder="C:/Users/jmerkle/Desktop/results", #an empty folder where results will be saved.
+                  set_seed=FALSE, # want to make results reproducible? Then set as TRUE
+                  save_at_each_iter=TRUE, #should all results be written to file at each time step?
+                  vertTransmission=0) #0 if false, 1 if true, vertical transmission of interactions
 
 
+# load up results files
+result.folder="C:/Users/jmerkle/Desktop/results"
+load(paste0(result.folder,"/interaction_matricies.RData"))
+load(paste0(result.folder,"/population_data.RData"))  #don't really need this if ind data is written out
+load(paste0(result.folder,"/individual_data.RData"))
+results <- read.csv(paste0(result.folder,"/population_stats.csv"))
+head(results)
 
-#plot fractions of informed and uninformed populations over time
-#png("pops_vs_time.png", width=6, height=4, units="in", res=400)
-par(mar=c(4,4,1,1))
-pop.info <- length(pop) * frac.informed
-pop.uninfo <- length(pop) * (1-frac.informed)
-ylim=range(c(pop.info, pop.uninfo))
-plot(time, pop.info, t="l", lwd=2, col="red", ylim=ylim, ylab="Population size")
-lines(time, pop.uninfo, lwd=2, col="blue")
-legend("topleft", legend=c("informed pop.", "uniformed pop."), lwd=2, col=c("red","blue"), bty="n")
-dev.off()
-
-#plot median age
-#png("med_age_vs_time.png", width=6, height=4, units="in", res=400)
-par(mar=c(4,4,1,1))
-plot(time, med.age, t="l", lwd=2, ylab="Median age")
-dev.off()
+# png(paste0(result.folder,"/summary_stats.png"), width=4, height=6, units="in", res=400)
+par(mfrow=c(3,2), mai=c(.3,.3,.03,.03), mgp=c(1,.1,0),
+    tck=-0.02, cex.axis=.8)
+plot(results$t, results$pop.size, type="l",ylab="N", xlab="Time step", lwd=3)
+plot(results$t, results$births, type="l",ylab="Births", xlab="Time step", lwd=3)
+plot(results$t, results$deaths, type="l",ylab="Deaths", xlab="Time step", lwd=3)
+plot(results$t, results$med.age, type="l",ylab="Median age", xlab="Time step", lwd=3)
+plot(results$t, results$frac.informed, type="l", 
+     ylim=c(0,1),ylab="Proportion", xlab="Time step", lwd=3)
+lines(results$t, 1-results$frac.informed, col="grey",lwd=3)
+legend("right", c("Informed","Uninformed"), lty=1, col=c("black","grey"), inset=0.02)
+# dev.off()
 
 #plot of one yrs interactions
 interactGraph <- graph_from_incidence_matrix(interactions[[2]], multiple = FALSE, mode="all", weighted = TRUE)
--- info.transfer.IBM.r
+++ info.transfer.IBM.r
@@ -1,3 +1,7 @@
+# Function to run information transfer IBM
+# developed by Zach Bell and Jerod Merkle
+# last update: June 2019
+
 
 info.transfer.IBM <- function(h=0.10, #increase in probability of death for uninformed
                               nl=0.01, # naive learning probability
@@ -7,8 +11,12 @@ info.transfer.IBM <- function(h=0.10, #increase in probability of death for unin
                               t=100, # time of simulation
                               sex.ratio=0.5, #what is the sex ratio of of the population/births?
                               age.distr.lamba=5, # lambda value for starting age distribution based on poison distribution
-                              
+                              informed.distr.beta=c(.5, 1), # probability of knowing information, beta distribution ranges from 0 to 1 (vector of 2 values: shape1 and shape2)
+                              bold.distr.beta=c(2, 2), # probability of being bold, beta distribution (vector of 2 values: shape1 and shape2)
                               birthdeath.file="C:/Users/jmerkle/Documents/GitHub/Memory_IBM/ageClass_Test.csv", #dataframe of age based birth and death rate
+                              result.folder="C:/Users/jmerkle/Desktop/results", #an empty folder where results will be saved.
+                              set_seed=TRUE, # want to make results reproducible? Then set as TRUE
+                              save_at_each_iter=TRUE, #should it write all results to file at each time step?
                               vertTransmission=0){ #0 if false, 1 if true, vertical transmission of interactions
 
   #manage packages
@@ -17,6 +25,16 @@ info.transfer.IBM <- function(h=0.10, #increase in probability of death for unin
   require(igraph)
   require(Matrix)
 
+  t1 <- Sys.time()
+  print(paste0("start time: ",t1,"."))
+  
+  #check the empty directories
+  if(dir.exists(result.folder)==FALSE){
+    dir.create(result.folder)
+  }
+  if(length(dir(result.folder))> 0)
+    print("Warning! Your result.folder has something in it. Those files will be overwritten!")
+  
   # bring in the age and birth and death rate data
   if(file.exists(birthdeath.file)==FALSE)
     stop("You didn't provide an existing file for birthdeath.file!")
@@ -27,16 +45,20 @@ info.transfer.IBM <- function(h=0.10, #increase in probability of death for unin
   maxAgeClass=max(d$ageClass) #max age class in order to get a proportion of age class for age class based rates of naive learning
   
   #create starting individual w attributes ("alive", "age", "informed"), initial population
-  set.seed(1)
+  print("Creating initial individual database.")
+  if(set_seed){
+    set.seed(1)
+  }
+  
   ind <- vector(mode="list", N0) 
   for(i in seq(ind)){
     ind[[i]]$alive <- 1 #0 if false, 1 if true
-    ind[[i]]$sex <- rbinom(1, 1, sex.ratio) #coinflip for sex, 0 if false, 1 if true  #NOTE FROM JAM: is False female or male? I also changed this to binom
+    ind[[i]]$sex <- rbinom(1, 1, sex.ratio) #coinflip for sex, 0 = male, 1 = female  
     ind[[i]]$age <- rpois(1, age.distr.lamba) #poisson distribution of age centered around 5
-    informedProb <- rbeta(1, .5, 1) #probability of knowing information, beta distribution ranges from 0 to 1
+    informedProb <- rbeta(1, informed.distr.beta[1], informed.distr.beta[2]) #probability of knowing information, beta distribution ranges from 0 to 1
     ageClass <- d$ageClass[which(d$age == ind[[i]]$age)]
     ind[[i]]$informed <- round(informedProb + informedProb * (ageClass/maxAgeClass)) #0 if false, 1 if true, modified by age class proportion 
-    ind[[i]]$boldness <- rbeta(1, 2, 2) #beta distribution, ranges from 0 to 1
+    ind[[i]]$boldness <- rbeta(1, bold.distr.beta[1], bold.distr.beta[2]) #beta distribution, ranges from 0 to 1
     ind[[i]]$mother <- 0
     ind[[i]]$birthYr <- 0
     #ind[[i]]$maternalSurvivalMod <- rbeta(1, .8, 1)
@@ -45,10 +67,7 @@ info.transfer.IBM <- function(h=0.10, #increase in probability of death for unin
   interactionMatrix <- Matrix(data = 0,
                               nrow = length(seq(ind)),
                               ncol = length(seq(ind)), sparse = TRUE)
-  
-  # g <- graph_from_adjacency_matrix(interactionMatrix, mode="undirected")
-  # plot(g, vertex.size=10, vertex.label=NA)
-  
+
   #make empty vectors to record population statistics for each time step
   time <- seq(t+1)
   
@@ -56,7 +75,7 @@ info.transfer.IBM <- function(h=0.10, #increase in probability of death for unin
   pop[[1]] <- which(sapply(ind, function(x) x$alive) == 1)
   
   frac.informed <- NaN * time # fraction of population that is informed
-  info <- sapply(ind, function(x) x$informed)
+  info <- sapply(ind, function(x) x$informed)   #NOTE: once we get the informed values fixed so they are only 0 or 1, we can simply do a mean here.
   frac.informed[1] <- sum(info  == 1) / length(info)
   
   med.age <- NaN * time
@@ -66,28 +85,28 @@ info.transfer.IBM <- function(h=0.10, #increase in probability of death for unin
   interactions <- list()
   interactions[[1]] <- interactionMatrix
   
+  tosave <- data.frame(time.stamp=Sys.time(), t=0, pop.size=length(pop[[1]]),births=NA, deaths=NA,
+                       frac.informed=frac.informed[1], med.age=med.age[1])
   
-  #simulation
-  #save.alive.only <- TRUE # optional cropping of "ind" to include alive individuals only 
-  t1 <- Sys.time()
+  #simulation starts here
+  print(paste0("Looping through the ", t, " years."))
   for(i in seq(t)){ # loop for each time increment
-    
-    
+    #prep that years data
     is.alive <- which(sapply(ind, function(x) x$alive) == 1) #just alive individuals from total list
     boldness <- sapply(ind[is.alive], function(x) x$boldness) #boldness of alive individuals
-    interactionMatrix <- Matrix(data = 0,
+    interactionMatrix <- Matrix(data = 0,   #build a new interaction matrix for this time step!
                                 nrow = length(seq(is.alive)),
                                 ncol = length(seq(is.alive)), sparse = TRUE)
     for(j in is.alive){ #loop for each alive individual
       
       curIndividual <- ind[[j]] #assigns current individual
-      indexJ <- which(is.alive == j)
+      indexJ <- which(is.alive == j)    #NOTE: Is indexJ aways the same value as j?
       ageClass <- d$ageClass[which(d$age == curIndividual$age)] #age class of current
       birthRate <- d$birthRate[which(d$age == curIndividual$age)] #birth rate of current age class
       survivalRate <- d$survivalRate[which(d$age == curIndividual$age)] #survival rate of current age class
       
       naiveLearn <- runif(1) <= (nl * ageClass/maxAgeClass * (1-curIndividual$informed)) # calculate a naive learning probability, depends on age class
-      if(naiveLearn){
+      if(naiveLearn){   #if this animal has knowledge, then add a 1 to informed column
         curIndividual$informed <- 1
       }
       
@@ -109,8 +128,11 @@ info.transfer.IBM <- function(h=0.10, #increase in probability of death for unin
       if(birth && (ind[[j]]$sex == 1)){ #checks for succesful birth and female sex
         #create new individual
         len.ind <- length(ind)
-        informedProb <- rbeta(1, .5, 1)
-        ind[[len.ind+1]] <- list(alive=1, age=1, sex = round(rnorm(1, .5, .25)), informed= round(vertTransmission * informedProb + informedProb * (1/maxAgeClass)), boldness = rbeta(1, 2, 2), mother = j, birthYr = i) # create offspring, inherits informed status of parent
+        informedProb <- rbeta(1, informed.distr.beta[1], informed.distr.beta[2])
+        ind[[len.ind+1]] <- list(alive=1, age=1, sex = round(rnorm(1, .5, .25)), 
+                                 informed=round(vertTransmission * informedProb + informedProb * (1/maxAgeClass)), 
+                                 boldness = rbeta(1, bold.distr.beta[1], bold.distr.beta[2]), 
+                                 mother = j, birthYr = i) # create offspring, inherits informed status of parent
         #makes new row in interaction matrix for new individual with inheirited interactions
         inheiritedConnections <- vector(nrow(interactionMatrix), mode = "numeric")
         inheiritedConnections[which(interactionMatrix[indexJ,] <= 1)] <- 1
@@ -136,7 +158,9 @@ info.transfer.IBM <- function(h=0.10, #increase in probability of death for unin
     #   if(length(is.dead) > 0) ind <- ind[-is.dead]
     # }
     
-    # saving Population stats for the time step
+    # saving Population stats for final output
+    numb.died <- sum(is.alive %in% which(sapply(ind, function(x) x$alive) == 1) == FALSE)
+    
     is.alive <- which(sapply(ind, function(x) x$alive) == 1)
     pop[[i+1]] <- is.alive 
     
@@ -148,18 +172,32 @@ info.transfer.IBM <- function(h=0.10, #increase in probability of death for unin
     
     interactions[[i+1]] <- interactionMatrix 
     
+    tosave <- rbind(tosave, data.frame(time.stamp=Sys.time(), t=i, pop.size=length(pop[[i+1]]),frac.informed=frac.informed[i+1],
+                                       births=length(which(sapply(ind, function(x) x$birthYr) == i)), 
+                                       deaths=numb.died, med.age=med.age[i+1]))
+    write.csv(tosave, paste0(result.folder,"/population_stats.csv"), row.names = FALSE)
+    
+    #save out all the main files?
+    if(save_at_each_iter == TRUE){
+      save(interactions, file = paste0(result.folder,"/interaction_matricies.RData"))
+      save(pop, file = paste0(result.folder,"/population_data.RData"))  #don't really need this if ind data is written out
+      save(ind, file = paste0(result.folder,"/individual_data.RData"))
+    }
+  
     #simulation progress
     Sys.sleep(.1)
-    print(paste(i, "of", t, "finished", "[", i/t*100, "%]"))
+    print(paste0(i, " of ", t, " finished ", "[", round(i/t*100,0), "%]"))
     
-  }
+  } # end of loop through t
+  
+  #write out final files
+  write.csv(tosave, paste0(result.folder,"/population_stats.csv"), row.names = FALSE)
+  save(interactions, file = paste0(result.folder,"/interaction_matricies.RData"))
+  save(pop, file = paste0(result.folder,"/population_data.RData"))  #don't really need this if ind data is written out
+  save(ind, file = paste0(result.folder,"/individual_data.RData"))
+  
   #time to run simulation
-  t2 <- Sys.time()
-  dt <- t2-t1
-  dt
-  
-  return()!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  
+  return(paste0("Completed in ", round((as.numeric(Sys.time())-as.numeric(t1))/60,2), " mins! Check your result.folder for results."))
 }
 
 
