bb0422557fc36858dd7016086227364d3c66e887
zbell1 <44716950+zbell1@users.noreply.github.com>
2020-01-21 13:26:49 -0700

    para code

--- Microtus arvalis/code2run_para.R
+++ Microtus arvalis/code2run_para.R
@@ -4,8 +4,17 @@
 
 
 #source the function
-source("C:/Users/Zach/Documents/GitHub/Memory_IBM/Memory_IBM/info.transfer.IBM.R")
+source("C:/Users/Zach/Documents/GitHub/Memory_IBM/Memory_IBM/info.transfer.IBM.para.R")
 source("C:/Users/Zach/Documents/GitHub/Memory_IBM/Memory_IBM/plotInfoTransferIBM.R")
+birthdeath.file <- "C:/Users/Zach/Documents/GitHub/Memory_IBM/Memory_IBM/Microtus arvalis/ageClass_MicroArv.csv" #dataframe of age based birth and death rate for FEMALES only. The columns should be age, ageClass, birthRate, and survivalRate, in that order.
+  if(file.exists(birthdeath.file)==FALSE){
+    stop("You didn't provide an existing file for birthdeath.file!")
+  }
+  d <- read.csv(file = birthdeath.file, colClasses = c("numeric", "numeric", "numeric", "numeric")) 
+  if(ncol(d)!= 4){
+    stop("Your birthdeath.file does not have 4 columns. The columns should be age, ageClass, birthRate, and survivalRate, in that order.")
+  }
+  
 args <- list(0.20, #increase in probability of death for uninformed
              c(0.01, .05, .10, .15, .20), # naive learning probability of the oldest animals (i.e., the ones that have the highest naive learning)
              2, # maximum mean (i.e., lambda of poison distribution) number of interactions per pair (if animal has 1 bold, it interacts with an animal with 1 boldness, and population is at or above K, this is the lambda of the interaction distributions)
@@ -17,7 +26,6 @@ args <- list(0.20, #increase in probability of death for uninformed
              1.5, # lambda value for starting age distribution based on poison distribution
              ".5 5", # starting probability distribution of knowing information; beta distribution ranges from 0 to 1 (vector of 2 values: shape1 and shape2)
              "2 5", # starting probability distribution of being bold, beta distribution (vector of 2 values: shape1 and shape2)
-             "C:/Users/Zach/Documents/GitHub/Memory_IBM/Memory_IBM/Microtus arvalis/ageClass_MicroArv.csv", #dataframe of age based birth and death rate for FEMALES only. The columns should be age, ageClass, birthRate, and survivalRate, in that order.
              "C:/Users/Zach/Desktop/results/MicroArv", #an empty folder where results will be saved.
              FALSE, # want to make results reproducible? Then set as TRUE
              TRUE, #should all results be written to file at each time step?
@@ -26,15 +34,22 @@ args <- list(0.20, #increase in probability of death for uninformed
              .4)
 
 args <- expand.grid(args)
+
 time1 <- Sys.time()
 results.folders <- paste("C:/Users/Zach/Desktop/results/MicroArv", 1:nrow(args), sep = "/")
-require(snowfall)
 sfInit(parallel = TRUE, cpus = 2)
-sfLibrary(igraph, Matrix, stringr)
-sfExport("args", "info.tansfer.IBM")
-sfClusterApplyLB(1:nrow(args), info.transfer.IBM)
+require(snowfall)
+require(Matrix)
+require(igraph)
+require(stringr)
+sfLibrary(igraph)
+sfLibrary(Matrix)
+sfLibrary(stringr)
+sfExport("args", "d")
+sfClusterApplyLB(1:nrow(args), info.transfer.IBM, d)
 sfExport("results.folders", "plotInfoTransferIBM")
 sfClusterApplyLB(1:length(results.folders), plotInfoTransferIBM)
+sfStop()
 time2 <- Sys.time()
 Sys.sleep(.001) 
 print(time2 - time1)
